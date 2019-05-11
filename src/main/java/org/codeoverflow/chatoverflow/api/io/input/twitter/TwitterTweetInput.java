9d06688270a54c777358f9811a1fdf9f15afd76a
Tobias Manske <tobias.manske@mailbox.org>
2019-05-11 23:58:14 +0200

    [WIP] TwitterConnector - Can send Tweet now
    Signed-off-by: Tobias Manske <tobias.manske@mailbox.org>

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/twitter/TwitterTweetInput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/twitter/TwitterTweetInput.java
@@ -2,4 +2,7 @@ package org.codeoverflow.chatoverflow.api.io.input.twitter;
 
 import org.codeoverflow.chatoverflow.api.io.input.Input;
 
-public interface TwitterTweetInput extends Input {}
+public interface TwitterTweetInput extends Input {
+    String getTimeLine();
+    Boolean sendTweet(String status);
+}
