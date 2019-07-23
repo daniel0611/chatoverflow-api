4bf446bc776906ca35840e180be65e8b413a54c1
Adrian Nöthlich <git@promasu.tech>
2019-07-23 06:46:35 +0200

    Regenerate and fixed requirements file
    Signed-off-by: Adrian Nöthlich <git@promasu.tech>

--- src/main/java/org/codeoverflow/chatoverflow/api/plugin/configuration/Output.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/plugin/configuration/Output.java
@@ -4,8 +4,8 @@ package org.codeoverflow.chatoverflow.api.plugin.configuration;
 
 import org.codeoverflow.chatoverflow.api.io.output.FileOutput;
 import org.codeoverflow.chatoverflow.api.io.output.SerialOutput;
-import org.codeoverflow.chatoverflow.api.io.output.chat.DiscordChatOutput;
 import org.codeoverflow.chatoverflow.api.io.output.chat.TwitchChatOutput;
+import org.codeoverflow.chatoverflow.api.io.output.chat.DiscordChatOutput;
 
 // THIS FILE IS GENERATED WHILE COMPILING. DO NOT CHANGE ANYTHING HERE!
 
@@ -29,7 +29,7 @@ public class Output {
      * @return the requirement object. Use the get() method only at runtime!
      */
     public Requirement<FileOutput> file(String uniqueRequirementId, String displayName, boolean isOptional) {
-        return requirements.requireInput(uniqueRequirementId, displayName, isOptional, FileOutput.class);
+        return requirements.requireOutput(uniqueRequirementId, displayName, isOptional, FileOutput.class);
     }
 
     /**
@@ -39,7 +39,7 @@ public class Output {
      * @return the requirement object. Use the get() method only at runtime!
      */
     public Requirement<FileOutput> file(String uniqueRequirementId) {
-        return requirements.requireInput(uniqueRequirementId, "File", false, FileOutput.class);
+        return requirements.requireOutput(uniqueRequirementId, "File", false, FileOutput.class);
     }
 
     /**
@@ -51,7 +51,7 @@ public class Output {
      * @return the requirement object. Use the get() method only at runtime!
      */
     public Requirement<SerialOutput> serial(String uniqueRequirementId, String displayName, boolean isOptional) {
-        return requirements.requireInput(uniqueRequirementId, displayName, isOptional, SerialOutput.class);
+        return requirements.requireOutput(uniqueRequirementId, displayName, isOptional, SerialOutput.class);
     }
 
     /**
@@ -61,29 +61,7 @@ public class Output {
      * @return the requirement object. Use the get() method only at runtime!
      */
     public Requirement<SerialOutput> serial(String uniqueRequirementId) {
-        return requirements.requireInput(uniqueRequirementId, "Serial", false, SerialOutput.class);
-    }
-
-    /**
-     * Requires a discord chat bot which has to be specified by the user.
-     *
-     * @param uniqueRequirementId a plugin unique identifier which is stored for your plugin
-     * @param displayName         a string to display to the user while setting your requirement
-     * @param isOptional          true if this requirement is optional, false if mandatory
-     * @return the requirement object. Use the get() method only at runtime!
-     */
-    public Requirement<DiscordChatOutput> discordChat(String uniqueRequirementId, String displayName, boolean isOptional) {
-        return requirements.requireInput(uniqueRequirementId, displayName, isOptional, DiscordChatOutput.class);
-    }
-
-    /**
-     * Requires a discord chat bot which has to be specified by the user.
-     *
-     * @param uniqueRequirementId a plugin unique identifier which is stored for your plugin
-     * @return the requirement object. Use the get() method only at runtime!
-     */
-    public Requirement<DiscordChatOutput> discordChat(String uniqueRequirementId) {
-        return requirements.requireInput(uniqueRequirementId, "Discord Chat", false, DiscordChatOutput.class);
+        return requirements.requireOutput(uniqueRequirementId, "Serial", false, SerialOutput.class);
     }
 
     /**
@@ -95,7 +73,7 @@ public class Output {
      * @return the requirement object. Use the get() method only at runtime!
      */
     public Requirement<TwitchChatOutput> twitchChat(String uniqueRequirementId, String displayName, boolean isOptional) {
-        return requirements.requireInput(uniqueRequirementId, displayName, isOptional, TwitchChatOutput.class);
+        return requirements.requireOutput(uniqueRequirementId, displayName, isOptional, TwitchChatOutput.class);
     }
 
     /**
@@ -105,7 +83,29 @@ public class Output {
      * @return the requirement object. Use the get() method only at runtime!
      */
     public Requirement<TwitchChatOutput> twitchChat(String uniqueRequirementId) {
-        return requirements.requireInput(uniqueRequirementId, "Twitch Chat", false, TwitchChatOutput.class);
+        return requirements.requireOutput(uniqueRequirementId, "Twitch Chat", false, TwitchChatOutput.class);
+    }
+
+    /**
+     * Requires a discord chat bot which has to be specified by the user.
+     *
+     * @param uniqueRequirementId a plugin unique identifier which is stored for your plugin
+     * @param displayName         a string to display to the user while setting your requirement
+     * @param isOptional          true if this requirement is optional, false if mandatory
+     * @return the requirement object. Use the get() method only at runtime!
+     */
+    public Requirement<DiscordChatOutput> discordChat(String uniqueRequirementId, String displayName, boolean isOptional) {
+        return requirements.requireOutput(uniqueRequirementId, displayName, isOptional, DiscordChatOutput.class);
+    }
+
+    /**
+     * Requires a discord chat bot which has to be specified by the user.
+     *
+     * @param uniqueRequirementId a plugin unique identifier which is stored for your plugin
+     * @return the requirement object. Use the get() method only at runtime!
+     */
+    public Requirement<DiscordChatOutput> discordChat(String uniqueRequirementId) {
+        return requirements.requireOutput(uniqueRequirementId, "Discord Chat", false, DiscordChatOutput.class);
     }
 
 
